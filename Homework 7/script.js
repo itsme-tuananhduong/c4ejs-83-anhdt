731e0bfa1eef1f63702eebc2da9dda985faa0ba8
Tuan Anh Duong <57260220+DaLast1@users.noreply.github.com>
2020-06-08 23:35:50 +0700

    Update script.js

--- Homework 7/script.js
+++ Homework 7/script.js
@@ -1,9 +1,36 @@
-let lst = document.getElementById("player-list");
-let p1 = `<li>Ronaldo</li>`,
-  p2 = `<li>Messi</li>`,
-  p3 = `<li>Neymar</li>`;
-lst.innerHTML += p1;
-lst.innerHTML += p2;
-lst.innerHTML += p3;
+// let person = {
+//   myName: "Duong Tuan Anh",
+//   myClass: "C4EJS83"
+// };
+// console.log(person.myName);
+// console.log(person.myClass);
 
-p3.remove();
+// // bai 1
+// let pList = document.getElementById("playername-1");
+// let p1 = `<li>Ronaldo</li>`,
+//     p2 = `<li>Messi</li>`,
+//     p3 = `<li id="li-3">Neymar</li>`;
+// pList.innerHTML += p1;
+// pList.innerHTML += p2;
+// pList.innerHTML += p3;
+// let p = document.getElementById("li-3");
+// p.remove();
+
+// // bai 2
+// let button = document.getElementById("button");
+// button.addEventListener('click', function() {
+//     let pList = document.getElementById("playername-2");
+//     let p = `<li>Kante</li>`
+//     pList.innerHTML += p;
+// })
+    
+// // bai 3
+// let input = document.getElementById("input");
+// input.addEventListener('keypress', function(data) {
+//     if (data.key == "Enter") {
+//         let pList = document.getElementById("playername-2");
+//         let p = `<li id="add-player"></li>`;
+//         pList.innerHTML += p;
+//         document.getElementById("add-player").innerHTML = input.value;
+//     }
+// })
